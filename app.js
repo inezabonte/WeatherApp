e2dbbdb3685c0454124f3e9e0399a7cda2a10be1
inezabonte <inezabonte@gmail.com>
2020-08-01 11:10:38 +0200

    added notifications

--- app.js
+++ app.js
@@ -3,3 +3,17 @@ if('serviceWorker' in navigator){
       .then((reg) => console.log('service worker registered', reg))
       .catch((err) => console.log('service worker not registered', err));
   }
+
+  Notification.requestPermission(function(status) {
+    console.log('Notification permission status:', status);
+});
+
+function displayNotification() {
+  if (Notification.permission == 'granted') {
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      reg.showNotification('Hello world!');
+    });
+  }
+}
+
+displayNotification();
