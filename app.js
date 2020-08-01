807716a0f220fd624aac8ada7b892f2fc475e7dc
inezabonte <inezabonte@gmail.com>
2020-08-01 11:47:29 +0200

    Notifications removed

--- app.js
+++ app.js
@@ -3,7 +3,3 @@ if('serviceWorker' in navigator){
       .then((reg) => console.log('service worker registered', reg))
       .catch((err) => console.log('service worker not registered', err));
   }
-
-  Notification.requestPermission((status) => {
-    console.log('Notification permission status:', status)
-  })
